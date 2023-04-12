ffef7b7ad6bbb8806c88fbc59ca723a0ef206ff5
serah wanjiru <WanjiruMburuuu@gmail.com>
2023-04-12 07:19:14 +0300

    day2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 fun main() {
     var x=modulus(55,30)
     println(x)
+    passwords()
 
 }
 //Create and invoke a function that given 2 numbers returns the remainder of their
@@ -8,4 +9,17 @@ fun main() {
 fun modulus(num1:Int,num2: Int):Int{
     var remainder=num1%num2
     return remainder
+}
+//Write a program in Kotlin that asks the user to enter a password.
+//If the password is "password123", print "Access granted".
+//If the password is incorrect, print "Access denied".
+fun passwords() {
+    print("Enter your password:")
+    var password = readLine()
+    if (password == "password123") {
+        println("Access granted")
+    }
+    else{
+        println("Access denied")
+    }
 }
