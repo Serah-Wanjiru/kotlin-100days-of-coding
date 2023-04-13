34dbbdaf6d14a1bb91df74aad03fbce251948112
serah wanjiru <WanjiruMburuuu@gmail.com>
2023-04-13 22:51:45 +0300

    day-4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,7 @@ fun main() {
     passwords()
     user()
     user()
+    numbers()
 
 }
 //Create and invoke a function that given 2 numbers returns the remainder of their
@@ -39,5 +40,27 @@ fun user(){
             println("odd")
         }
     }
+
+
+//Write a function that prints each number from 1 to 100 on a new line. For each
+//multiple of 3, print "Fizz" instead of the number. For each multiple of 5, print
+//"Buzz" instead of the number. For numbers which are multiples of both 3 and
+//5, print "FizzBuzz" instead of the number.
+fun numbers(){
+    for (i in 1..100)
+        if (i%3==0 && i%5==0){
+            println("FizzBuzz")
+        }
+    else if (i%3==0){
+        println("Fizz")
+        }
+        else if (i%5==0){
+            println("Buzz")
+        }
+    else{
+        println(i)
+        }
+
+}
 
 
